ad0d54a20c8a8a6c02066b260bfc15c118d9abb9
Luan <2023017361@aluno.restinga.ifrs.edu.br>
2026-03-30 22:26:17 -0300

    30/03

--- auto.bat
+++ auto.bat
@@ -3,11 +3,5 @@ pip install mysqlclient
 pip install -r "requirements.txt"
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
-ifrs
-
-ifrs
-ifrs
-y
 python wrapper_pessoas.py
 python manage.py runserver
